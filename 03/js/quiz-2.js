fa32d6d23aceceb7cfbed9058bc399f411a5876d
songyunjeong <yungunaa@gmail.com>
2023-07-05 23:14:09 +0900

    ~137p

--- 03/js/quiz-2.js
+++ 03/js/quiz-2.js
@@ -1,9 +1,9 @@
-let number = parseInt(prompt("1보다 큰 숫자를 입력하세요."));
+let number = parseInt(prompt('1보다 큰 숫자를 입력하세요.'));
 let sum = 0;
 
 if (number !== null && number > 1) {
   for (let i = 1; i <= number; i++) {
-    if (i % 2 == 1) {
+    if (i % 2 != 0) {
       continue;
     }
     sum += i;
--- 04/js/example-1.js
+++ 04/js/example-1.js
@@ -1,5 +1,22 @@
+// 방법(1)
+/*
 function multiple(a, b) {
-    return a * b;
+  return a * b;
 }
 
 console.log(`두 수를 곱한 결과는 ${multiple(10, 20)}입니다.`);
+*/
+
+// 방법(2)
+function multiple(a, b) {
+  return a * b;
+}
+
+let number = prompt('두 수를 입력하세요.', '2 13');
+number = number.split(' ');
+
+number[0] = Number(number[0]);
+number[1] = Number(number[1]);
+console.log(typeof number[0], typeof number[1]);
+
+console.log(`두 수를 곱한 결과는 ${multiple(number[0], number[1])}입니다.`);
